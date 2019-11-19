d14e5a8d742bd2922a5be3e176bd06a5ffad04b9
Barry Tarlton <btarlton@gmail.com>
2019-11-18 23:40:11 -0500

    cleaned up code to remove commented and unneeded logic

--- targetDetector.py
+++ targetDetector.py
@@ -20,16 +20,11 @@ chan0 = AnalogIn(mcp, MCP.P0)
 print('Raw ADC Value: ', chan0.value)
 print('ADC Voltage: ' + str(chan0.voltage) + 'V')
 
-last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 7400     # to keep from being jittery we'll only change
-		# volume when the pot has moved a significant amount
-		# on a 16-bit ADC
+hit_tolerance = 7400     # amount of vibration to count as a hit.
 
 
 while True:
-	# we'll assume that the pot didn't move
-	vibration_value_changed = False
-
+	
 	# read the analog pin
 	vibration_value = chan0.value
 
@@ -37,19 +32,12 @@ while True:
 
 #	print('ADC Voltage: ' + str(chan0.voltage) + 'V')
 	print('Channel VALUE=' + str(vibration_value)) 
-	# how much has it changed since the last read?
-	# pot_adjust = abs(vibration_value - last_read)
 
-	if vibration_value > tolerance:
-	    vibration_value_changed = True
+	if vibration_value > hit_tolerance:
+	    
+	    print('Big Hit value!!!!!!!!!!!!!!!! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ' + str(vibration_value))
+        
+		# Broadcast an event
 
-	if vibration_value_changed:
-
-	    #for x in range(1000): 
-            # print('Changed value = {change}%' .format(change = pot_adjust))
-            print('Big Hit value!!!!!!!!!!!!!!!! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  ' + str(vibration_value))
-	    # save the potentiometer reading for the next loop
-            # last_read = vibration_value
-
-	    # hang out and do nothing for a half second if hit
-            time.sleep(0.5)
+	    # hang out and do nothing for a second if hit
+        time.sleep(1)
